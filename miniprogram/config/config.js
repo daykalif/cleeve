edfc14d1967ef5ec01c161d59ee888aeda1f8767
daykalif <1038200595@qq.com>
2024-06-15 15:01:56 +0800

    feat: 阶段1，01周，3-1配置文件与第一次调用服务端API

---
+++ miniprogram/config/config.js
@@ -0,0 +1,13 @@
+const config = {
+    appkey: 'ma6OHiFMc6gVyd5h',
+    apiBaseUrl: 'http://se.7yue.pro/v1/ '
+    // http://se.7yue.pro/v1/
+    // http://192.168.0.102:8080/v1/
+    // https://course.talelin.com/v1/
+}
+
+export {
+    config
+}
+
+
---
+++ miniprogram/mock/home/names.js
@@ -0,0 +1,12 @@
+export const data1 = [{
+    "description": "秋天是金色的。麦穗是金色的，秋阳是金色的。虽然冬快至，但秋依旧。",
+    "entrance_img": "https://course.talelin.com/hero.png",
+    "extend": null,
+    "id": 1,
+    "internal_top_img": "http://i2.sleeve.7yue.pro/m33.png",
+    "name": "t-1",
+    "online": true,
+    "title": "清凉一夏，折扣季",
+    "title_img": null,
+    "tpl_name": null
+}];
