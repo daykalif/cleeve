7103c6241d080fa1c678341d94d1f34f9e557cbc
daykalif <1038200595@qq.com>
2024-06-30 18:50:11 +0800

    refactor: 添加一份可测试的数据

--- models/spu.js
+++ models/spu.js
@@ -1,11 +1,13 @@
 import {Http} from "../utils/http";
+import {spuData} from "../mock/home/spu";
 
 class Spu {
     /** wjp-flow：第一步：创建spu数据请求方法 */
     static getDetail(id) {
-        return Http.request({
+        const data = Http.request({
             url: `spu/id/${id}/detail`
         });
+        return data ?? spuData;
     }
 }
 
