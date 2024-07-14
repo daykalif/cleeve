a5b37bc13d1e752b19cb94173bcd743feecb8161
daykalif <1038200595@qq.com>
2024-07-14 17:54:34 +0800

    feat: 阶段1:小程序高级电商前端，07周.高级电商订单管理开发与前端项目收尾，3-16: 删除购物车子项与全选问题探讨

--- components/cart-item/index.js
+++ components/cart-item/index.js
@@ -36,13 +36,7 @@ Component({
             const online = Cart.isOnline(cartItem);
             const stockPinch = Cart.stockPinch(cartItem);
             this.setData({
-                specStr,
-                discount,
-                soldOut,
-                online,
-                stockPinch,
-                stock: cartItem.sku.stock,
-                skuCount: cartItem.count,
+                specStr, discount, soldOut, online, stockPinch, stock: cartItem.sku.stock, skuCount: cartItem.count,
             });
         }
     },
@@ -50,5 +44,17 @@ Component({
     /**
      * 组件的方法列表
      */
-    methods: {}
+    methods: {
+        /** 点击购物车删除按钮时，触发的事件 */
+        onDelete(event) {
+            let skuId = this.properties.cartItem.sku.id;
+            let cart = new Cart();
+            cart.removeItem(skuId);
+            this.setData({
+                cartItem: null
+            });
+            // 抛出删除事件，供父组件计算总价
+            this.triggerEvent("itemdelete", {skuId});
+        },
+    }
 })
