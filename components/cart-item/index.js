7ffcdd6137d40d992e722bfab90663b94bfc99f3
daykalif <1038200595@qq.com>
2024-07-14 14:14:54 +0800

    feat: 阶段1:小程序高级电商前端，07周.高级电商订单管理开发与前端项目收尾，3-7: 三种状态的显示

--- components/cart-item/index.js
+++ components/cart-item/index.js
@@ -1,24 +1,20 @@
 // components/cart-item/index.ts
 Component({
 
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        cartItem: Object,
+    },
 
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {},
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+    /**
+     * 组件的方法列表
+     */
+    methods: {}
 })
--- components/checkbox/index.js
+++ components/checkbox/index.js
@@ -1,4 +1,4 @@
-// components/check-box/index.ts
+// components/checkbox/index.ts
 Component({
     /**
      * 组件的属性列表
