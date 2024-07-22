2d66006e2ddd66e88cc22094dd03c9ef2e0941a3
daykalif <1038200595@qq.com>
2024-07-23 00:40:26 +0800

    feat: 阶段1:小程序高级电商前端，07+周.购物车价格同步、优惠券、下单与微信支付，4-6: 收货地址组件（2）显示用户收货地址

--- components/address/index.js
+++ components/address/index.js
@@ -1,24 +1,42 @@
 // components/address/index.ts
+import boolean from "../../miniprogram_npm/lin-ui/common/async-validator/validator/boolean";
+
 Component({
 
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {},
 
-  },
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        address: Object,
+        hasChosen: false,
+    },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        onChooseAddress(event) {
+            this.getUserAddress();
+        },
 
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
+        async getUserAddress() {
+            let res;
+            try {
+                res = await wx.chooseAddress({});
+            } catch (e) {
+                console.log(e);
+            }
+            if (res) {
+                this.setData({
+                    address: res,
+                    hasChosen: true,
+                })
+            }
+        }
+    }
 })
